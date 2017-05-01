bd91d4503a123997fe47d9b4d55de50fcc3f0e91
drericstrong <dr.eric.strong@gmail.com>
2017-04-30 21:48:05 -0400

    Updating dist
    Distribution for version 0.14.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name='pypbe',
   packages=['pypbe'],
-  version='0.13',
+  version='0.14',
   description='Module to calculate Point Buy Equivalent for Tabletop RPG',
   author='Eric Strong',
   author_email='dr.eric.strong@gmail.com',
   url='https://github.com/drericstrong/pypbe',
-  download_url='https://github.com/drericstrong/pypbe/tarball/0.13',
+  download_url='https://github.com/drericstrong/pypbe/tarball/0.14',
   keywords=['RPG', 'Pathfinder', 'D&D', 'Tabletop', 'Point Buy', 'Point Buy Equivalent'],
   classifiers=[],
 )
